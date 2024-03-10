7c76d0c5a3b80477e9f5915f2b3cf6bfb172532e
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-03-10 12:30:55 +0530

    123

---
+++ coverpage.js
@@ -0,0 +1,17 @@
+count = 0;
+function muimagegalary(){
+    imageContentarray =['Welcome','Ok go there','Its different']
+    imageArray=['https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/home-landing-web/Homepage_Banners_1920x655_38aEuqC.jpg?format=webp&w=1300&dpr=1.5',
+'https://blog.elverys.ie/app/uploads/2018/06/NIKE-BANNERS-1920X696-2-1.jpg',
+'https://storage.sg.content-cdn.io/in-resources/7f703506-689d-4b4e-b482-c32d60769d33/Images/ProductImages/Source/Home%20Page%20Banner%20images/Home%20Banner%203-Earpods.jpeg']
+    document.getElementById('imagegalary').src=imageArray[count];
+    //document.getElementById('imagecontent').innerHTML = imageContentarray[count];
+    if(count>=2){
+        count = 0;
+    }else{
+    count++;
+    }
+}
+muimagegalary();
+setInterval(muimagegalary,5000);
+
