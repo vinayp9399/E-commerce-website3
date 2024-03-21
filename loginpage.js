eaa7de9618fd4ebbbaf6609bad20215069c23bb6
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-03-21 22:51:12 +0530

    jshjs

---
+++ loginpage.js
@@ -0,0 +1,10 @@
+
+var fname = document.getElementById('fname');
+let lname = document.getElementById("lname");
+let password = document.getElementById("password");
+let email = document.getElementById("email");
+let mob_no = document.getElementById("mob_no");
+
+function submit(){
+    console.log(fname.value);
+}
